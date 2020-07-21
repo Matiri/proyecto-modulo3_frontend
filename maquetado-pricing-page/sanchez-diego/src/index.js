5785150a6c623b7d1398e53bf66307b6e6e12ae5
Diego Sanchez <diegominepro@gmail.com>
2020-07-21 17:53:10 -0300

    trabajando en el MainTitle

--- maquetado-pricing-page/sanchez-diego/src/index.js
+++ maquetado-pricing-page/sanchez-diego/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MainTitle from './components/MainTitle.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainTitle />
   </React.StrictMode>,
   document.getElementById('root')
 );
