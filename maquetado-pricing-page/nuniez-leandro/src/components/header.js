b2eae10a8d8af72716d45385561284e91f9b9615
Leandro-matea <leandronuniez14@gmail.com>
2020-07-23 15:44:02 -0300

    agregué node_modules

--- maquetado-pricing-page/nuniez-leandro/src/components/header.js
+++ maquetado-pricing-page/nuniez-leandro/src/components/header.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import '../styles/header.css';
 
 class Header extends React.Component {
-    render(){
-        return(
-            <div>hola</div>
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a id="" className="navbar-brand" href="#">Company name</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link titulo" href="#"> <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">FEATURES</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">ENTERPRISE</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">SUPPORT</a>
+                        </li>
+                        <li className="nav-item">
+                            <button type="button" class="btn btn-light">LOGIN</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
--- maquetado-pricing-page/nuniez-leandro/src/index.js
+++ maquetado-pricing-page/nuniez-leandro/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from './components/header'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Header />,
   document.getElementById('root')
 );
 
