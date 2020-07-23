2ae0f30028fcedbc39308074e54e75c975660864
Alan Nacimiento <alan21gtaonline@gmail.com>
2020-07-23 17:50:03 -0300

    agregue estilos a card

--- maquetado-pricing-page/nacimiento-alan/src/components/Card.js
+++ maquetado-pricing-page/nacimiento-alan/src/components/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "../styles/Card.css"
 class Card extends React.Component{
     render(){
         const url = 'aaa';
         return(
-            <div>
-                <h2>{this.props.titulo}</h2>
-                <h2>{this.props.precio}</h2>
+            <div className="Tarjeta">
+                <h3>{this.props.titulo}</h3>
+                <h1>{this.props.precio}</h1> <h5>/mo</h5>
                 <p>
                     {this.props.descripcion}
                 </p>
--- maquetado-pricing-page/nacimiento-alan/src/index.js
+++ maquetado-pricing-page/nacimiento-alan/src/index.js
@@ -7,9 +7,14 @@ import  Card from "./components/Card"
 
 ReactDOM.render(
   <Card 
-        titulo="Pro" 
-        descripcion = "hsdwshdgwjdhgd"
-        nombreBoton = "Boton" 
+        titulo="Pro"
+        precio="$0" 
+        descripcion = 
+        "10 users included 
+        2 GB of storage
+        Help center access
+        Email support"
+        nombreBoton = "SING UP FOR FREE" 
   />,
   document.getElementById('root')
 );
