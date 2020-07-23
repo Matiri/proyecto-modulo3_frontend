210918f8203bfb5149e36f31b64a5169f5231c5d
Florencia <natashaflorencia109@gmail.com>
2020-07-23 16:37:21 -0300

    card free completo

--- maquetado-pricing-page/AlcaideFlorencia/src/Components/CardFree.js
+++ maquetado-pricing-page/AlcaideFlorencia/src/Components/CardFree.js
@@ -5,23 +5,26 @@ import '../Styles/CardFree.css';
 class CardFree extends React.Component{
     render(){
         return(
-            <div className='containerFree'>
+            <div className='container-principal'>
                 <div className='container_title'>
-                    <h1 className='title_free'>Free</h1>
+                    <span>Free</span>
                 </div>
-            
-            
-                    <h1>$0</h1>/mo
                 
-                <div className='container-li'>
-                    <li>10 users included</li>
-                    <li>2 GB of storage</li>
-                    <li>Help center access</li>
-                    <li>Email support</li>
-                </div>                
-                <button>Sing up for free</button>
+                <div className='containerFree'>
+                        <h1 className='price-number'>$0<span className='text-diference'>/mo</span></h1>
+                    
+                    <div className='container-li'>
+                        <li>10 users included</li>
+                        <li>2 GB of storage</li>
+                        <li>Help center access</li>
+                        <li>Email support</li>
+                    </div>                
+                    <button>Sing up for free</button>
+                </div>
             </div>
-
+            
+            
+            
         )
     }
 }
