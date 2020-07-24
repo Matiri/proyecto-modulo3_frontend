8b14926275c93e034f8ca0e06af797ccafe0c841
Fockxs <facugsil@gmail.com>
2020-07-24 15:58:11 -0300

    CardPro terminada

--- maquetado-pricing-page/silveirafacundo/src/components/CardPro.js
+++ maquetado-pricing-page/silveirafacundo/src/components/CardPro.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import './../assets/styles/CardPro.css'
 class CardPro extends React.Component {
     render(){
         return(
             <div className="CardPro__main-div">
-                <div className="CardFree__first-div">
-                    <h3 className="CardFree__title">Pro</h3>
-                    <p className="CardPro__paragraph">Most popular</p>
+                <div className="CardPro__first-div">
+                    <h3 className="CardPro__title">Pro</h3>
+                    <p className="CardPro__status">Most popular</p>
                 </div>
-                <div className="CardFree__second-div">
-                    <h2 className="CardFree__per-month">$0/mo</h2>
-                    <p className="CardFree__paragraph">20 users included 10 GB of storage, Help center access and Priority email support</p>
+                <div className="CardPro__second-div">
+                    <h2 className="CardPro__per-month">$0/mo</h2>
+                    <p className="CardPro__paragraph">20 users included 10 GB of storage, Help center access and Priority email support</p>
                     <form action="">
-                        <button type="button" className="CardFree__button">SIGN UP FOR FREE</button>
+                        <button type="button" className="CardPro__button">GET STARTED</button>
                     </form>
                 </div>
             </div>
