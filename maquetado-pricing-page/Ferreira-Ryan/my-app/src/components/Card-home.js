46532f35dbe50ef8df6a1e5105e83ceea2a774bb
Ry-Ferreira <ryferreira1@outlook.es>
2020-07-24 18:03:16 -0300

    {FINISH}-cards-buttons

--- maquetado-pricing-page/Ferreira-Ryan/my-app/src/components/Card-home.js
+++ maquetado-pricing-page/Ferreira-Ryan/my-app/src/components/Card-home.js
@@ -3,51 +3,51 @@ import '../styles/cards.css';
 
 function CardHome() {
     return(
-        <div className="cards-contain">
+        <div className="cards-contain" >
             <div className="card-left card">
-                <div  className="text-top">
-                    <h3>Free</h3>
+                <div className="text-top">
+                    <h2>Free</h2>
                 </div>
                 <div className="text-in">
-                    <p>$0/mo</p>
+                    <p className="price">$0/mo</p>
                     <div><p>20 users included</p></div>
                     <div><p>10 GB of storage</p></div>
                     <div><p>Help center access</p></div>    
                     <div><p>Priority email  support</p></div>
                 </div>
                 <div className="btn-contain">
-                    <button className="free-btn">SIGN UP FOR FREE</button>
+                    <button className="btn">SIGN UP FOR FREE</button>
                 </div>
             </div>
             <div className="card-middle card">
                 <div className="text-top">
-                    <h3>Pro</h3>
+                    <h2>Pro</h2>
                     <p>Most popular</p>
                 </div>
                 <div className="text-in">
-                    <p>$15/mo</p>
+                    <p className="price">$15/mo</p>
                     <div><p> 20 users included</p></div>
                     <div><p>10 GB of storage</p></div>
                     <div><p>Help center access</p></div>
                     <div><p>Priority email  support</p></div>
                 </div>
                 <div className="btn-contain">
-                    <button className="free-btn">GET STARTED</button>
+                    <button className="btn btn-pro">GET STARTED</button>
                 </div>
             </div>
             <div className="card-right card">
                 <div className="text-top">
-                    <h3>Enterprise</h3>
+                    <h2>Enterprise</h2>
                 </div>
                 <div className="text-in">
-                    <p>$15/mo</p>
+                    <p className="price">$15/mo</p>
                     <div><p>20 users included</p></div>
                     <div><p>10 GB of storage</p></div>
                     <div><p>Help center access</p></div>
                     <div><p>Priority email  support</p></div>
                 </div>
                 <div className="btn-contain">
-                    <button className="free-btn">CONTACT US</button>
+                    <button className="btn">CONTACT US</button>
                 </div>
             </div>
         </div>
