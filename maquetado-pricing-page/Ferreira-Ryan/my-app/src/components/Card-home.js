a92c1108380376e24f5f7dd4772e10861d16cbda
Ryan <ryferreira1@outlook.es>
2020-07-23 22:21:44 -0300

    change-css-style-for-cards

--- maquetado-pricing-page/Ferreira-Ryan/my-app/src/components/Card-home.js
+++ maquetado-pricing-page/Ferreira-Ryan/my-app/src/components/Card-home.js
@@ -4,55 +4,80 @@ import '../styles/cards.css';
 function CardHome() {
     return(
         <div className="cards-contain">
-            <div className="card-left">
-                <div>
+            <div className="card-left card">
+                <div  className="text-top">
                     <h3>Free</h3>
                 </div>
-                <div>
+                <div className="text-in">
                     <h1>$0/mo</h1>
-                    <p>
-                        10 users included
-                        2 GB of storage
-                        Help center access
-                        Email support
-                    </p>
+                    <div className="text-in">
+                        <p>
+                            20 users included
+                        </p>
+                        <p>
+                            10 GB of storage
+                        </p>
+                        <p>
+                            Help center access
+                        </p>
+                        <p>
+                            Priority email  support
+                        </p>
+                    </div>
                 </div>
                 <div className="btn-contain">
                     <button className="free-btn">SIGN UP FOR FREE</button>
                 </div>
             </div>
-            <div className="card-middle">
-                <div>
+            <div className="card-middle card">
+                <div className="text-top">
                     <h3>Pro</h3>
+                    <p>Most popular</p>
                 </div>
-                <div>
+                <div className="text-in">
                     <h1>$15/mo</h1>
-                    <p>
-                        20 users included
-                        10 GB of storage
-                        Help center access
-                        Priority email support
-                    </p>
+                    <div className="text-in">
+                        <p>
+                            20 users included
+                        </p>
+                        <p>
+                            10 GB of storage
+                        </p>
+                        <p>
+                            Help center access
+                        </p>
+                        <p>
+                            Priority email  support
+                        </p>
+                    </div>
                 </div>
                 <div className="btn-contain">
                     <button className="free-btn">GET STARTED</button>
                 </div>
             </div>
-            <div className="card-right">
-                <div>
+            <div className="card-right card">
+                <div className="text-top">
                     <h3>Enterprise</h3>
                 </div>
-                <div>
+                <div className="text-in">
                     <h1>$15/mo</h1>
-                    <p>
-                        50 users included
-                        30 GB of storage
-                        Help center access
-                        Phone & email support
-                    </p>
+                    <div className="text-in">
+                        <p>
+                            20 users included
+                        </p>
+                        <p>
+                            10 GB of storage
+                        </p>
+                        <p>
+                            Help center access
+                        </p>
+                        <p>
+                            Priority email  support
+                        </p>
+                    </div>
                 </div>
                 <div className="btn-contain">
-                    <button className="free-btn">GET STARTED</button>
+                    <button className="free-btn">CONTACT US</button>
                 </div>
             </div>
         </div>
