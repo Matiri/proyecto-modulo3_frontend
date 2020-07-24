268e236d79f8251190167070b569f314364f80e9
LeandroNuñez <leandronuniez14@gmail.com>
2020-07-23 22:35:27 -0300

    Title-styled

--- maquetado-pricing-page/nuniez-leandro/src/components/Title.js
+++ maquetado-pricing-page/nuniez-leandro/src/components/Title.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../styles/Title.css'
 
 class Title extends React.Component {
-    render(){
-        return(
-            <div className="text-center">
-                <h1 id="titulo">Pricing</h1>
-                <p id="content">Quickly build an effective pricing table for your potential customers with this layout. It's built with default Material-UI components with little customization.</p>
+    render() {
+        return (
+            <div className="row justify-content-center">
+                <div className="col-7 text-center" id="hola">
+                    <h1 id="titulo"><b>Pricing</b></h1>
+                    <p id="content">Quickly build an effective pricing table for your potential customers with this layout. It's built with default Material-UI components with little customization.</p>
+                </div>
             </div>
         )
     }
