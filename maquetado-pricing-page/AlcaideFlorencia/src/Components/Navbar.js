5246311b4a840dd93529241089c3b1893076f0f4
Florencia <natashaflorencia109@gmail.com>
2020-07-23 21:37:23 -0300

    navbar completo

--- maquetado-pricing-page/AlcaideFlorencia/src/Components/Navbar.js
+++ maquetado-pricing-page/AlcaideFlorencia/src/Components/Navbar.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import Navbar from '../Styles/Navbar.css'
+import  '../Styles/Navbar.css'
 
 class Navbar extends React.Component{
     render(){
-        return{
+        return(
+            <div className='container-principal'>
+                
+                <h4>Company name</h4>
 
-        }
+                <nav className='navigation'>
+                    <ul>
+                        <li><a href='#'>FEATURES</a></li>
+                        <li><a href='#'>ENTERPRISE</a></li>
+                        <li><a href='#'>SUPPORT</a></li>
+                        <li><button>Login</button></li>
+                    </ul>
+                    
+                </nav>
+            </div>
+        )
+            
+        
     }
 }
 export default Navbar;
