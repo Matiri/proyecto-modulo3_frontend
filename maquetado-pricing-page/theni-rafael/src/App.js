370c06e966bf43761e0649e6d2ec1fbc39a50c8e
rtheni <rntm72@gmail.com>
2020-07-24 12:36:45 -0300

    Mejora de css

--- maquetado-pricing-page/theni-rafael/src/App.js
+++ maquetado-pricing-page/theni-rafael/src/App.js
@@ -3,25 +3,27 @@ import Card from './components/Card';
 import favImg from './img/fav.png';
 import './App.css';
 
-const card1 = { title: "Free", imgsrc:"", text1: "", price: "0", text2: "10 users included 2 GB of storage Help center access Email support", button: "SIGN UP FOR FREE" }
-const card2 = { title: "Pro", imgsrc:{favImg}, text1: "Most Pupular", price: "15", text2: "20 users included 10 GB of storage Help center access Priority email support", button: "GET STARTED" }
-const card3 = { title: "Enterprise", imgsrc:"", text1: "", price: "30", text2: "50 users included 30 GB of storage Help center access Email support", button: "CONTACT US" }
+const card1 = { title: "Free", imgsrc:"", text1: "", price: "0", text2: "10 users included 2 GB of storage Help center access Email support", button: "SIGN UP FOR FREE", classnam:"cardBtnEmpty"}
+const card2 = { title: "Pro", imgsrc:{favImg}, text1: "Most Popular", price: "15", text2: "20 users included 10 GB of storage Help center access Priority email support", button: "GET STARTED", classnam:"cardBtnFull"}
+const card3 = { title: "Enterprise", imgsrc:"", text1: "", price: "30", text2: "50 users included 30 GB of storage Help center access Email support", button: "CONTACT US", classnam:"cardBtnEmpty"}
 
 function App() {
   return (
     <div>
       <header>
         <p>Company name</p>
-        <a><p>FEATURES</p></a>
-        <a><p>ENTERPRISE</p></a>
-        <a><p>SUPPORT</p></a>
-        <input type="button" value="LOGIN" href="#"></input>
+        <div className="headerLinks">
+          <a href="#"><p>FEATURES</p></a>
+          <a href="#"><p>ENTERPRISE</p></a>
+          <a href="#"><p>SUPPORT</p></a>
+          <input type="button" value="LOGIN" href="#" className="headerLinksBtn"></input>
+        </div>
       </header>
-      <h1>Pricing</h1>
-      <h2>Quickly build an effective pricing table for your potential customers with this layout. It's built with default Material-UI components with little customization.</h2>
-      <Card title={card1.title} price={card1.price} text2={card1.text2} button={card1.button}/>
-      <Card title={card2.title} imgsrc={card2.imgsrc} price={card2.price} text1={card2.text1} text2={card2.text2} button={card2.button}/>
-      <Card title={card3.title} price={card3.price} text2={card3.text2} button={card3.button}/>
+      <h1 className="headerTitle">Pricing</h1>
+      <h2 className="headerSubtitle">Quickly build an effective pricing table for your potential customers with this layout. It's built with default Material-UI components with little customization.</h2>
+      <Card title={card1.title} price={card1.price} text2={card1.text2} button={card1.button} classnam={card1.classnam}/>
+      <Card title={card2.title} imgsrc={card2.imgsrc} price={card2.price} text1={card2.text1} text2={card2.text2} button={card2.button}  classnam={card2.classnam}/>
+      <Card title={card3.title} price={card3.price} text2={card3.text2} button={card3.button} classnam={card3.classnam}/>
       <footer>
         <h3>Company</h3>
         <ul>
