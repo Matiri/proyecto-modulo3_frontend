10dbb9a0d4bfd7a2b2c9cf76d30c5e325074c370
Nahuel <axelbenerozzo12@gmail.com>
2020-07-25 16:04:06 -0300

    create cardlist and footer with cardlist inside

--- maquetado-pricing-page/benerozzo-nahuel/src/index.js
+++ maquetado-pricing-page/benerozzo-nahuel/src/index.js
@@ -3,19 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import MainTitle from './componentes/MainTitle';
-import Card from './componentes/Card';
-import NavBar from './componentes/NavBar';
-
+// import MainTitle from './componentes/MainTitle';
+// import Card from './componentes/Card';
+// import NavBar from './componentes/NavBar';
+// import CardList from './componentes/cardList';
+// import Footer from './componentes/footer';
+import Page from './pages/pages.js';
 ReactDOM.render(
   <React.StrictMode>
     {/* <Card precio=" 14 " user="10" storage="2" support="" button="sign up for free" titleCard="FREE" /> */}
-    {<NavBar
+    {/* {<NavBar
     companyName=" Company Name " 
     navFeatures=" features " 
     navEnterprise=" enterprise " 
     navSupport=" support " 
-    navButtonText=" login "/>}
+    navButtonText=" login "/>} */}
+    {/* {<CardList
+  cardListTitle="Company"
+  cardFirstList="Team"
+  cardSecondList=" History "
+  cardThirdList=" Contact us "
+  cardFourthList=" location "
+  cardFifthList="" />} */}
+  {<Page />}
+  {<Footer copyright="Your Website"/>}
   </React.StrictMode>,
   document.getElementById('root')
 );
