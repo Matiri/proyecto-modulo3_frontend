047cbc620308a26fa6fd6cbcf8a0a3558080481e
Alan 21 Code <alan21naci@gmail.com>
2020-07-26 20:12:18 -0300

    Modifique navbar

--- maquetado-pricing-page/nacimiento-alan/src/components/Navbar.js
+++ maquetado-pricing-page/nacimiento-alan/src/components/Navbar.js
@@ -5,15 +5,21 @@ import "../styles/Navbar.css";
 class Navbar extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                        <a class="nav-item nav-link" href="#">Features</a>
-                        <a class="nav-item nav-link" href="#">Pricing</a>
-                        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </div>
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand">Company name</a>
+                <div class="navbar">
+                    <ul class="nav justify-content-end">
+                        <li>
+                            <a class="navbar text-dark" href="#">FEATURES</a>
+                        </li>
+                        <li>
+                            <a class="navbar text-dark" href="#">ENTERPRISE</a>
+                        </li>
+                        <li>
+                            <a class="navbar text-dark" href="#">SUPPORT</a>
+                        </li>
+                        <button type="button" class="btn btn-primary btn-sm">LOGIN</button>
+                    </ul>
                 </div>
             </nav>
         )
