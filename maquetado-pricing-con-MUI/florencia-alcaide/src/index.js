d816a24bf9ceda103890af3530320454adeaf0a9
Flor Alcaide <natashaflorencia109@gmail.com>
2020-07-30 12:38:27 -0300

    cards

--- maquetado-pricing-con-MUI/florencia-alcaide/src/index.js
+++ maquetado-pricing-con-MUI/florencia-alcaide/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Navbar from './Components/Navbar';
 import MainTitle from './Components/MainTitle'
+import Page from './Components/Page'
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar title='Company name'/>
     <MainTitle/>
+  
+    <Page/>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
