25063842328d6dca32b39de280a70d8c37eed617
Nahuel Benerozzo <axelbenerozzo12@gmail.com>
2020-10-01 17:57:18 -0300

    frontend commenced

--- proyecto-modulo3_frontend/src/App.js
+++ proyecto-modulo3_frontend/src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [songs, setSongs] = useState([]);
+
+  const getUsers = async (e, users) => {
+    const response = await fetch('http://localhost:4000');
+    const data = await response.json();
+  setUsers(data)
+  }
+
+  const showFavorites = async(e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:4000/songs');
+    const data = await response.json();
+    setSongs(data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>First Name</TableCell>
+            <TableCell>Last Name</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Age</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+          {users.map(users => <TableCell key = {users._id}>{users.firstName}</TableCell>)}
+          {users.map(users => <TableCell key = {users._id}>{users.lastName}</TableCell>)}
+          {users.map(users => <TableCell key = {users._id}>{users.email}</TableCell>)}
+          {users.map(users => <TableCell key = {users._id}>{users.age}</TableCell>)}
+          <TableCell>
+            <Button onClick = {showFavorites}></Button>
+          </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Album</TableCell>
+            <TableCell>Duration</TableCell>
+            <TableCell>Artist</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            {songs.map(songs => <TableCell key = {users.favorite}>{songs.name}</TableCell>)}
+            {songs.map(songs => <TableCell key = {users.favorite}>{songs.album}</TableCell>)}
+            {songs.map(songs => <TableCell key = {users.favorite}>{songs.duration}</TableCell>)}
+            {songs.map(songs => <TableCell key = {users.favorite}>{songs.artist}</TableCell>)}
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
